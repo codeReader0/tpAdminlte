aafe5dba58a5465dfbe09d6385368c96ccb11d57
zilong <songzl@sudyapp.com>
2019-07-10 18:39:16 +0800

    update

---
+++ application/admin/config/menu.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '用户管理' => 'admin/User/userList',
+    '后台账号及角色管理' => [
+        '账号管理' => 'admin/AdminUser/adminUserList',
+        '角色管理' => 'admin/AuthGroup/authGroupList',
+    ],
+];
